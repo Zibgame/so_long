f8335740b818e804100437be3f8c0af4da6c7796
zibgame <zcadinot@c1r1p12.42lehavre.fr>
2025-11-04 17:05:38 +0100

    42

---
+++ so_long.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zcadinot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 16:31:53 by zcadinot          #+#    #+#             */
+/*   Updated: 2025/11/04 16:56:14 by zcadinot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "mlx.h"
+
+# define KEY_ESC 65307
+# define GAME_NAME "So Long"
+# define KEY_W   119
+# define KEY_A   97
+# define KEY_S   115
+# define KEY_D   100
+
+typedef struct s_game
+{
+	void	*mlx;
+	void	*win; 
+}	t_game;
+
+int	quit(t_game *game);
+
+int handle_key(int keycode, t_game *game);
+
+int start_game(t_game *game);
+
+int		main(void);
+
+#endif
+
