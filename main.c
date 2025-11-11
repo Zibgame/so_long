bebb0bd1493494d0a099c1e278bc8e9ad0f3a271
Zibgame <fanizicy@gmail.com>
2025-11-11 21:52:48 +0100

    42

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: zcadinot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 15:42:52 by zcadinot          #+#    #+#             */
-/*   Updated: 2025/11/10 02:01:43 by zcadinot         ###   ########.fr       */
+/*   Updated: 2025/11/11 21:52:30 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main(int argc, char **argv)
 {
 	t_game	game;
-	if (check_arg(argc, argv) || check_map_valid(&argv[1]) == 0)
+	if (check_arg(argc, argv) || !(check_map_valid(&argv[1]) == 0))
 		return (1);
 	game = start_game(argv[1]);
 	mlx_loop(game.mlx);
