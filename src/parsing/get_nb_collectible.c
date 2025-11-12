e28dc4ae0f39e05f51543c6ce65144adf7d8837f
zibgame <zcadinot@c2r9p5.42lehavre.fr>
2025-11-12 09:27:03 +0100

    42

--- src/parsing/get_nb_collectible.c
+++ src/parsing/get_nb_collectible.c
@@ -6,7 +6,7 @@
 /*   By: zcadinot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 08:37:06 by zcadinot          #+#    #+#             */
-/*   Updated: 2025/11/12 09:13:41 by zcadinot         ###   ########.fr       */
+/*   Updated: 2025/11/12 09:19:58 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ int	get_nb_tile(char **grid, char c)
 	int	y;
 	int	*size;
 
-	x = 0;
-	y = 0;
 	nb = 0;
 	size = map_size(grid);
+	y = 0;
 	while (y < size[1])
 	{
+		x = 0;
 		while (x < size[0])
 		{
-			if (grid[x][y] == c)
+			if (grid[y][x] == c)
 				nb++;
 			x++;
 		}
@@ -35,3 +35,4 @@ int	get_nb_tile(char **grid, char c)
 	}
 	return (nb);
 }
+
