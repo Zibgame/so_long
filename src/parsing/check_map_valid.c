49f34fd167ed28a7cae207ea20658d7d6464eba8
zibgame <zcadinot@c2r9p5.42lehavre.fr>
2025-11-12 13:20:41 +0100

    42

--- src/parsing/check_map_valid.c
+++ src/parsing/check_map_valid.c
@@ -6,7 +6,7 @@
 /*   By: zcadinot <zcadinot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 01:06:37 by zcadinot          #+#    #+#             */
-/*   Updated: 2025/11/12 13:15:54 by zcadinot         ###   ########.fr       */
+/*   Updated: 2025/11/12 13:20:09 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ int	check_map_valid(char *path)
 	if ((get_nb_tile(grid, 'C') < 1) || (get_nb_tile(grid, 'E')  != 1) || (get_nb_tile(grid, 'P')  != 1))
 		return (0);
 	if (!check_map_size(grid))
+		return (0);
+	if (!check_path(grid))
 		return (0);
 	return (1);
 }
